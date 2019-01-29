5b614b4297a87f444fd1a2e1459d55e1c9a4614d
chemica1 <46700473+chemica1@users.noreply.github.com>
2019-01-29 17:06:20 +0900

    feat : item

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import Form from './components/Form';
+import TodoItemList from './components/TodoItemList';
 
 class App extends Component {
   render() {
     return (
-      <TodoTemplate>
-        템플릿 완성
+      <TodoTemplate form={<Form/>}>
+        <TodoItemList/>
       </TodoTemplate>
     );
   }
